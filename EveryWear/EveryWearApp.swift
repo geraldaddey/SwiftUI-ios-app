cfd2a832232470d4d648e3a4f06475b06df63643
geraldaddey <geraldaddey@yahoo.com>
2022-06-14 21:43:42 +0000

    Initial Commit

---
+++ EveryWear/EveryWearApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryWearApp.swift
+//  EveryWear
+//
+//  Created by Quansah on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct EveryWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

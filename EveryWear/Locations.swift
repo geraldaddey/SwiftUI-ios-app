70f2dd4c8d87ef72003caaa44d80964bd1ec08b2
geraldaddey <geraldaddey@yahoo.com>
2022-06-15 11:18:49 +0000

    decoded info from json file and rendered to content view

--- EveryWear/Locations.swift
+++ EveryWear/Locations.swift
@@ -11,7 +11,15 @@ import Foundation
 class Locations : ObservableObject {
     let places : [Location]
     
+    var primary: Location{
+        places[0]
+    }
+    
     init () {
         let url = Bundle.main.url(forResource: "locations", withExtension: "json")!
+        let data = try!Data(contentsOf: url)
+        let decoder = JSONDecoder()
+        places = try! decoder.decode([Location].self, from: data)
     }
 }
+ 
